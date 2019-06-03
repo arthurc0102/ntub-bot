52af6ebe213f55636eca611fcbeacb39a0b93627
Arthur Chang <arthurc0102@gmail.com>
2019-06-03 16:04:46 +0800

    fix: Update default retry

--- config/components/crawler.py
+++ config/components/crawler.py
@@ -1,3 +1,5 @@
+import requests
+
 # Path
 
 HOST = 'http://ntcbadm.ntub.edu.tw'
@@ -10,3 +12,6 @@ ASSESSMENT_TA_PATH = HOST + '/STDWEB/Assessment_Detail_TA.aspx'  # 教學助理
 # Message
 NOT_TIME_MESSAGE = '目前非期末教學評量時間'
 LOGIN_ERROR_MESSAGE = '閒置過久或尚未登入系統，請重新登入系統!'
+
+# Requests config
+requests.adapters.DEFAULT_RETRIES = 5  # 增加重試次數，避免連線失效
